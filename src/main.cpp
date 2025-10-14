5929e35d503b01a934b2bf21a5d204ece8a02401
Kamil <kamilzaripov228@gmail.com>
2025-10-14 18:25:27 +0300

    Added ResourceManager

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,23 @@
 
 #include <iostream>
 
+#include "Renderer/ShaderProgram.h"
+#include "Resources/ResourceManager.h"
+
+// array of coordinates
+GLfloat point[] = {
+     0.0f, 0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f
+};
+
+// array of colors
+GLfloat colors[] {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
 int g_windowSizeX = 640;
 int g_windowSizeY = 480;
 
@@ -19,7 +36,7 @@ void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int
     }
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
     /* Initialize the library */
     if (!glfwInit()) {
@@ -54,19 +71,57 @@ int main(void)
     std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
-    glClearColor(1, 0, 0, 1);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(pWindow))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        ResourceManager resourceManager(argv[0]);
+        auto pDefaultShaderProgram = resourceManager.loadShaders("DefaultShader", "res/shaders/vertex.txt", "res/shaders/fragment.txt");
+        if (!pDefaultShaderProgram) {
+            std::cerr << "Can't create shader program: " << "DefaultShader" << std::endl;
+            return -1;
+        }
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(pWindow);
+        GLuint points_vbo = 0;
+        glGenBuffers(1, &points_vbo); // Creates a buffer in video memory and writes it to points_vbo
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo); // Makes the buffer active 
+        glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW); // Loads an array of points into GPU video memory 
 
-        /* Poll for and process events */
-        glfwPollEvents();
+        GLuint colors_vbo = 0;
+        glGenBuffers(1, &colors_vbo); // Creates a buffer in video memory and writes it to colors_vbo
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo); // Makes the buffer active 
+        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW); // Loads an array of colors into GPU video memory
+
+        // Let's create a VAO instruction fot the GPU on how to interpret verties
+        GLuint vao = 0;
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+
+        // Connecting the vertex buffer to the shader
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        // Connecting the color buffer to the shader
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(pWindow))
+        {
+            /* Render here */
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            pDefaultShaderProgram->use();
+            glBindVertexArray(vao);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(pWindow);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
     }
 
     glfwTerminate();
